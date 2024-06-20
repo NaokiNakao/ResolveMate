909e776100cd1333b892b3ef4fb89805bc5ab88f
Nakao <naokinakao.nn@gmail.com>
2024-06-20 09:15:56 -0400

    Initial commit

---
+++ src/main/java/com/nakao/resolvemate/ResolveMateApplication.java
@@ -0,0 +1,13 @@
+package com.nakao.resolvemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResolveMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResolveMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nakao/resolvemate/ResolveMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nakao.resolvemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResolveMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
