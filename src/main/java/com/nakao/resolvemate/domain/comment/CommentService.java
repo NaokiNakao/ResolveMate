7d44a9312aeff5d6a27807114b31b3a8a1fb284c
Nakao <naokinakao.nn@gmail.com>
2024-06-29 23:28:56 -0400

    AOP for logs

--- src/main/java/com/nakao/resolvemate/domain/comment/CommentService.java
+++ src/main/java/com/nakao/resolvemate/domain/comment/CommentService.java
@@ -6,7 +6,6 @@ import com.nakao.resolvemate.domain.ticket.Ticket;
 import com.nakao.resolvemate.domain.ticket.TicketRepository;
 import com.nakao.resolvemate.domain.user.Role;
 import com.nakao.resolvemate.domain.user.User;
-import com.nakao.resolvemate.domain.util.LogService;
 import com.nakao.resolvemate.domain.util.SecurityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,15 +21,12 @@ public class CommentService {
     private final TicketRepository ticketRepository;
     private final CommentRepository commentRepository;
     private final SecurityService securityService;
-    private final LogService<CommentService> logService;
 
     public CommentDTO createComment(UUID ticketId, Comment comment) {
         Ticket ticket = getCurrentTicket(ticketId);
         verifyAuthorization(ticketId);
         comment.setTicket(ticket);
-        CommentDTO createdComment = CommentMapper.toDTO(commentRepository.save(comment));
-        logService.info(this, "Comment created: " + createdComment.getId());
-        return createdComment;
+        return CommentMapper.toDTO(commentRepository.save(comment));
     }
 
     public List<CommentDTO> getCommentsByTicketId(UUID ticketId) {
@@ -38,13 +34,9 @@ public class CommentService {
 
         verifyAuthorization(ticketId);
 
-        List<CommentDTO> comments = commentRepository.findAllByTicket(ticket).stream()
+        return commentRepository.findAllByTicket(ticket).stream()
                 .map(CommentMapper::toDTO)
                 .toList();
-
-        logService.info(this, "Found " + comments.size() + " comments for ticket " + ticketId);
-
-        return comments;
     }
 
     private void verifyAuthorization(UUID ticketId) {
@@ -52,19 +44,13 @@ public class CommentService {
 
         if (!ticketRepository.hasAccessToTicket(ticketId, currentUser.getId()) &&
                 !Objects.equals(currentUser.getRole(), Role.ADMIN)) {
-            String message = "Unauthorized access for ticket " + ticketId + " by user " + currentUser.getId();
-            logService.warn(this, message);
-            throw new ForbiddenAccessException(message);
+            throw new ForbiddenAccessException("Unauthorized access for ticket " + ticketId + " by user " + currentUser.getId());
         }
     }
 
     private Ticket getCurrentTicket(UUID ticketId) {
         return ticketRepository.findById(ticketId)
-                .orElseThrow(() -> {
-                    String message = "Ticket not found with ID: " + ticketId;
-                    logService.warn(this, message);
-                    return new ResourceNotFoundException(message);
-                });
+                .orElseThrow(() -> new ResourceNotFoundException("Ticket not found with ID: " + ticketId));
     }
 
 }
---
+++ src/main/java/com/nakao/resolvemate/infrastructure/logging/AttachmentServiceLogging.java
@@ -0,0 +1,23 @@
+package com.nakao.resolvemate.infrastructure.logging;
+
+import com.nakao.resolvemate.domain.attachment.AttachmentDTO;
+import com.nakao.resolvemate.domain.util.LogService;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class AttachmentServiceLogging {
+
+    private final LogService<AttachmentServiceLogging> logService;
+
+    @AfterReturning(pointcut = "execution(* com.nakao.resolvemate.domain.attachment.AttachmentService.createAttachment(..))",
+            returning = "result")
+    public void createAttachmentLog(AttachmentDTO result) {
+        logService.info(this, "Attachment created: " + result.getId());
+    }
+
+}
